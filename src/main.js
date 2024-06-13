ec94c9d4e46291f312025a2eb12a9f37f1fc7786
Kamran Kiani <kiani.km@gmail.com>
2024-06-13 12:27:09 +0330

    Add files

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
+const id = document.querySelector(".advice-id");
+const quote = document.querySelector(".quote");
+const btn = document.querySelector(".button");
 
+async function fetchAdvice() {
+  try {
+    const response = await fetch(
+      `https://api.adviceslip.com/advice?timestamp=${new Date().getTime()}`
+    );
+    const json = await response.json();
+
+    id.textContent = json.slip.id;
+    quote.textContent = json.slip.advice;
+  } catch (error) {
+    id.textContent = "";
+    quote.textContent = "Failed to fetch advice";
+    console.error("Error fetching advice: ", error);
+  }
+}
+
+btn.addEventListener("click", fetchAdvice);
+
+window.addEventListener("load", fetchAdvice);
